5831991e7b12e3e18c8d88bdbfd6e007e07cd474
Luca Fanselau <luca.fanselau@outlook.com>
2022-10-06 12:38:58 +0200

    Add example application that depends on library

---
+++ examples/demo/src/App.tsx
@@ -0,0 +1,12 @@
+import { a } from "dirigent";
+
+function App() {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">Hello, world!</p>
+      <p className="text-grey-700">Value of a is: {a}</p>
+    </div>
+  );
+}
+
+export default App;
