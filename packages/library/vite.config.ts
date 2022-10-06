3bf13dae9cb8f46b21679375f8ded5ed6b0e0654
Luca Fanselau <luca.fanselau@outlook.com>
2022-10-06 19:12:44 +0200

    Add linting to library

--- packages/library/vite.config.ts
+++ packages/library/vite.config.ts
@@ -1,5 +1,5 @@
-import react from "@vitejs/plugin-react";
 import path from "path";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
@@ -17,7 +17,7 @@ export default defineConfig({
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "dirigent",
       formats: ["es", "umd"],
-      fileName: (format) => `${pkg.name}.${format}.js`,
+      fileName: format => `${pkg.name}.${format}.js`,
     },
     rollupOptions: {
       external: Object.keys(pkg.peerDependencies),
