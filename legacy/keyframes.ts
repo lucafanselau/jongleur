4a7e564f946dcb9d097b2c440ce0104939e0595b
Luca Fanselau <luca.fanselau@outlook.com>
2022-10-13 12:38:50 +0200

    Move legacy code because of type checking

--- legacy/keyframes.ts
+++ legacy/keyframes.ts
@@ -1,4 +1,4 @@
-import type { ObjectStore } from "./states";
+import type { AnyField, ObjectStore } from "./states";
 import type {
   Keyframe,
   Keyframes,
@@ -14,9 +14,9 @@ import type {
  * @param timelines The definition for the single keyframes
  * @returns A parsed version of the keyframes, usable by the other hooks
  */
-export const orchestrate = <Scene extends ValidScene>(
+export const createKeyframes = <Base extends { [K: string]: {  } }(
   base: State<Scene>,
-  timelines: TimelineDefinition<Scene>,
+  timelines: TimelineDefinition<Scene>
 ): Keyframes<Scene> => {
   const allObjects: (keyof Scene)[] = Object.keys(base);
 
@@ -40,22 +40,21 @@ export const orchestrate = <Scene extends ValidScene>(
         if (index === 0) {
           prev = objBase;
           prevKeyframe = 0;
-        }
- else {
+        } else {
           prevKeyframe = keyframes[index - 1];
           prev = frames[prevKeyframe].state;
         }
 
         const fields = Object.keys(def) as Scene[keyof Scene][];
         if (fields.length === 0) {
-return {
+          return {
             ...frames,
             [frame]: {
               state: prev,
               interpolations: {},
             },
           };
-}
+        }
 
         // When there is something set active state
         active.push([prevKeyframe, frame]);
@@ -79,7 +78,7 @@ return {
           },
         };
       },
-      {},
+      {}
     );
 
     return {
@@ -98,3 +97,34 @@ return {
     allObjects,
   };
 };
+
+const create = <
+  S extends { [O: string]: { [K in AnyField]?: number } },
+  Keyframes extends { [O in keyof S]: { [frame: number]: S[O] } }
+>(
+  _s: S,
+  _keyframes: Keyframes
+) => {};
+
+create(
+  {
+    camera: {
+      position: 0,
+    },
+    other: {
+      intensity: 1,
+    },
+  },
+  {
+    camera: {
+      0: {
+        position: 4,
+      },
+    },
+    other: {
+      20: {
+        intensity: 2,
+      },
+    },
+  }
+);
