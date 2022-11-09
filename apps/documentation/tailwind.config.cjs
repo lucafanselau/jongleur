f86faf3e9ac5d69a03227cf44ad10d41bb0ae8bc
Luca Fanselau <luca.fanselau@outlook.com>
2022-11-09 23:32:38 +0100

    Even more progress on the custom layout

--- apps/documentation/tailwind.config.cjs
+++ apps/documentation/tailwind.config.cjs
@@ -8,20 +8,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto Flex", ...defaultTheme.fontFamily.sans]
+        sans: ["Roboto Flex", ...defaultTheme.fontFamily.sans],
       },
       gridTemplateColumns: {
         "md-layout": "16rem minmax(0, 1fr)",
-        layout: "16rem minmax(0, 64rem) 16rem"
+        layout: "16rem minmax(0, 64rem) 16rem",
       },
       colors: {
         "light-blue": colors.sky[300],
-        blue: colors.sky[600],
         "light-gray": colors.slate[300],
         "mid-gray": colors.slate[500],
-        "dark-gray": colors.slate[800]
-      }
-    }
+        "dark-gray": colors.slate[800],
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")]
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 };
