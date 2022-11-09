dccdbee1333e94062412077fd2b45f31782f8b79
Luca Fanselau <luca.fanselau@outlook.com>
2022-11-09 23:38:26 +0100

    🔥 Remove most of the template

--- apps/documentation/src/config.ts
+++ apps/documentation/src/config.ts
@@ -5,15 +5,15 @@ export const SITE = {
   url: "...",
 };
 
-export const OPEN_GRAPH = {
-  image: {
-    src: "https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true",
-    alt:
-      "astro logo on a starry expanse of space," +
-      " with a purple saturn-like planet floating in the right foreground",
-  },
-  twitter: "astrodotbuild",
-};
+// export const OPEN_GRAPH = {
+//   image: {
+//     src: "https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true",
+//     alt:
+//       "astro logo on a starry expanse of space," +
+//       " with a purple saturn-like planet floating in the right foreground",
+//   },
+//   twitter: "astrodotbuild",
+// };
 
 // This is the type of the frontmatter you put in the docs markdown files.
 export type Frontmatter = {
@@ -26,13 +26,7 @@ export type Frontmatter = {
   lang?: string;
 };
 
-export const KNOWN_LANGUAGES = {
-  English: "en",
-} as const;
-export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
-
 export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
-
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
 // See "Algolia" section of the README for more information.
@@ -40,19 +34,4 @@ export const ALGOLIA = {
   indexName: "XXXXXXXXXX",
   appId: "XXXXXXXXXX",
   apiKey: "XXXXXXXXXX",
-};
-
-export type Sidebar = Record<
-  typeof KNOWN_LANGUAGE_CODES[number],
-  Record<string, { text: string; link: string }[]>
->;
-export const SIDEBAR: Sidebar = {
-  en: {
-    "Section Header": [
-      { text: "Introduction", link: "en/introduction" },
-      { text: "Page 2", link: "en/page-2" },
-      { text: "Page 3", link: "en/page-3" },
-    ],
-    "Another Section": [{ text: "Page 4", link: "en/page-4" }],
-  },
 };
