f5dc218079d29dea12f1005cf5ddd7e8b18cd7cd
Luca Fanselau <luca.fanselau@outlook.com>
2023-02-07 13:30:53 +0100

    Update build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "tsup";
 
 const entries = {
   index: "src/index.ts",
-  timeline: "src/timeline/index.ts",
-  progress: "src/progress/index.ts",
-  scroll: "src/scroll/index.ts"
+  timeline: "src/timeline/index.ts"
+  // progress: "src/progress/index.ts",
+  // scroll: "src/scroll/index.ts"
 };
 
 export default defineConfig({
