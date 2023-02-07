5fbc9c228421b91ad042e7dd0b21dd609a4aa76a
Luca Fanselau <luca.fanselau@outlook.com>
2023-02-07 13:10:21 +0100

    Remove progress folder entirely

--- src/progress/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./register";
-export * from "./progress";
-export type { Interpolation } from "./interpolation";
-export { interpolate } from "./interpolation";
-export * from "./hooks";
