93c1df6ff67972e07b700f607278fb7db2acc9e5
Luca Fanselau <luca.fanselau@outlook.com>
2023-02-07 13:23:37 +0100

    Update seek to call invalidate

--- src/timeline/seek.ts
+++ src/timeline/seek.ts
@@ -1,7 +1,7 @@
-import { isSome } from "../utils";
+import { invalidate } from "@react-three/fiber";
 import type { ClipStore } from "./store";
 import type { FieldsBase, Seek, StateBase } from "./types";
-import { applyProgressToObject, findActiveClip, findConsideredClips } from "./utils";
+import { applyProgressToObject, findConsideredClips } from "./utils";
 
 export const createSeek = <Fields extends FieldsBase, Base extends StateBase<Fields>>(
   store: ClipStore<Fields, Base>
@@ -12,18 +12,20 @@ export const createSeek = <Fields extends FieldsBase, Base extends StateBase<Fie
     // apply the updates, applicable to range
     const range: [number, number] = [Math.min(last, progress), Math.max(last, progress)];
 
-    objects.forEach(o =>
-      keyframes[o].fields.forEach(f => {
-        const field = f as keyof Fields;
-        const clips = keyframes[o].clips[field];
-        const considered = findConsideredClips(range, clips);
-        const clip = findActiveClip(range[1], considered);
-        if (isSome(clip)) {
-          // apply directissimy
-          applyProgressToObject(store, progress, o, field, considered);
-        }
-      })
-    );
+    // we also keep track if we applied any of the clips
+    const appliedClip = objects
+      .flatMap(o =>
+        keyframes[o].fields.map(f => {
+          const field = f as keyof Fields;
+          const clips = keyframes[o].clips[field];
+          const considered = findConsideredClips(range, clips);
+          return applyProgressToObject(store, progress, o, field, considered);
+        })
+      )
+      .reduce((acc, curr) => acc || curr);
+
+    if (appliedClip) invalidate();
+
     const { setLastProgress } = store.getState();
     setLastProgress(progress);
   };
